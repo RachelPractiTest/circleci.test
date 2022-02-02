1b0e512bc7052720cff5392a1e7d980602088b5e
RachelPractiTest <43436917+RachelPractiTest@users.noreply.github.com>
2022-02-02 17:30:12 +0200

    Update test_report.clj

--- test/circleci/test/test_report.clj
+++ test/circleci/test/test_report.clj
@@ -62,7 +62,7 @@
             :end-test-var   [{:type :end-test-var, :var "Var"}]}
            @reports))
 
-    (is (= {:pass 1, :fail 2, :error 3} @counters))))
+    (is (= {:pass 1, :fail 2, :error 1} @counters))))
 
 (deftest ^:failing test-failing-metadata-skips
   (is false))
